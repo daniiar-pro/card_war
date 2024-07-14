2ea1dd87a56b03ba115ddfa7b309617b757e94d5
daniiar-pro <abdraiymovd@gmail.com>
2024-07-15 01:34:42 +0200

    initial program structure

---
+++ war.py
@@ -0,0 +1,21 @@
+class Cards:
+    def __init__(self,player_one, player_two):
+        self.player_one = player_one
+        self.player_two = player_two
+    
+    def get_player_card(self):
+        ...
+    
+    def is_card_valid(self):
+        ...
+    def draw(self):
+        ...
+    def winner_is(self):
+        ...
+
+def main():
+    card = Cards
+
+
+if __name__=="__main__":
+    main()
